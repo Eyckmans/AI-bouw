2476144a12f82df908dd2633c42ba91482d8c1f0
Eyckmans <nick.eyckmans@hotmail.be>
2026-07-18 16:03:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from api import customers
 
 app = FastAPI(
     title="BouwAI API",
     description="AI assistent voor de Belgische bouwsector",
     version="0.1"
 )
+
+
+app.include_router(
+    customers.router
+)
+
 
 @app.get("/")
 def home():
